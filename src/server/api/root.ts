c63a9e7eb87595d63cfec453eff842a900cb2f35
mikepodsy <podolioukh.michael@gmail.com>
2025-06-24 20:43:26 -0400

    fix: add dummy health router to prevent empty appRouter issues

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { createCallerFactory, createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -6,6 +6,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  health: publicProcedure.query(() => "ok"),
   // Add other routers here
 });
 
